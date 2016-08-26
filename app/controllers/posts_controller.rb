8b560493dfb00caaf9f4654f0c04859164be047b
Sam Lin <greenlin111@gmail.com>
2016-08-26 09:45:45 +0800

    create new, edit ,show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,42 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, :except => [:index]
 
+	#GET posts/index
+	#GET /posts
 	def index
-		@posts = Post.all
+		if params[:id]
+			@post = Post.find(params[:id])
+		else
+			@post = Post.new
+		end
+			@posts = Post.all
+	end
+	#GET posts/new
+	def new 
 		@post = Post.new
 	end
 
+
+  #POST /posts
 	def create 
 		@post = Post.new(params_permitted)
-		@post.save
+		@post.user = current_user
+		if @post.save
 			flash[:notice]= "新增成功"
-			redirect_to posts_path
-		# redirect_to post_path(@post)
+			redirect_to post_path(@post)
+		else
+			@posts = Post.all
+			render :action => :index
+	end
 	end
 
+	#GET /posts/:id
 	def show
-		@post = Post.find(@post)
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
 	end
 
 	private
