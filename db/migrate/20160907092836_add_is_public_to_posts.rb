7c31a61b9948c2d670b5ea1b28bb5062e42e3569
Sam Lin <greenlin111@gmail.com>
2016-09-30 22:17:57 +0800

    revise migration data

--- db/migrate/20160907092836_add_is_public_to_posts.rb
+++ db/migrate/20160907092836_add_is_public_to_posts.rb
@@ -1,4 +1,4 @@
-class Add < ActiveRecord::Migration[5.0]
+class AddIsPublicToPosts < ActiveRecord::Migration[5.0]
   def change
   	add_column :posts, :is_public, :boolean, :default => :false
   end
