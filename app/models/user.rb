f6ad4fc60b6a244c2047d230856dec65cf5fcdf1
Sam Lin <greenlin111@gmail.com>
2016-10-04 15:15:48 +0800

    add style on devise and revise facebook and google log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,6 +72,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     # Case 1: Find existing user by facebook uid
     user = User.find_by_fb_uid( auth.uid )
+    byebug
     if user
       user.fb_token = auth.credentials.token
       #user.fb_raw_data = auth
@@ -94,19 +95,20 @@ class User < ApplicationRecord
     user.fb_uid = auth.uid
     user.fb_token = auth.credentials.token
     user.email = auth.info.email
+    user.nickname = auth.info.name
     user.password = Devise.friendly_token[0,20]
     #user.fb_raw_data = auth
     user.save!
     return user
   end
 
-  def self.from_omniauth(access_token)
+  def self.from_omniauth_google(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
-      user = User.create(name: data["name"],
+      user = User.create(nickname: data["name"],
              email: data["email"],
              password: Devise.friendly_token[0,20])
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,110 +12,110 @@
 
 ActiveRecord::Schema.define(version: 20160919062738) do
 
-  create_table "friendships", force: :cascade do |t|
+  create_table "friendships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
     t.string   "status",     default: "pending"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id"], name: "index_friendships_on_user_id"
+    t.index ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
+    t.index ["user_id"], name: "index_friendships_on_user_id", using: :btree
   end
 
-  create_table "groups", force: :cascade do |t|
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "post_groupships", force: :cascade do |t|
+  create_table "post_groupships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "post_id"
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_post_groupships_on_group_id"
-    t.index ["post_id"], name: "index_post_groupships_on_post_id"
+    t.index ["group_id"], name: "index_post_groupships_on_group_id", using: :btree
+    t.index ["post_id"], name: "index_post_groupships_on_post_id", using: :btree
   end
 
-  create_table "post_likeships", force: :cascade do |t|
+  create_table "post_likeships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "user_id"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_post_likeships_on_post_id"
-    t.index ["user_id"], name: "index_post_likeships_on_user_id"
+    t.index ["post_id"], name: "index_post_likeships_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_post_likeships_on_user_id", using: :btree
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "title"
-    t.text     "content"
+    t.text     "content",            limit: 65535
     t.integer  "user_id"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.integer  "replies_count",      default: 0
+    t.boolean  "is_public",                        default: false
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.integer  "replies_count",                    default: 0
     t.datetime "last_replies"
-    t.integer  "page_views",         default: 0
-    t.boolean  "is_public",          default: false
+    t.integer  "page_views",                       default: 0
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
 
-  create_table "replies", force: :cascade do |t|
+  create_table "replies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "user_id"
-    t.text     "comment"
+    t.text     "comment",    limit: 65535
     t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table "taggings", force: :cascade do |t|
+  create_table "taggings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "tag_id"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_taggings_on_post_id"
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["post_id"], name: "index_taggings_on_post_id", using: :btree
+    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
   end
 
-  create_table "tags", force: :cascade do |t|
+  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_tags_on_name"
+    t.index ["name"], name: "index_tags_on_name", using: :btree
   end
 
-  create_table "user_postships", force: :cascade do |t|
+  create_table "user_postships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "post_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_user_postships_on_post_id"
-    t.index ["user_id"], name: "index_user_postships_on_user_id"
+    t.index ["post_id"], name: "index_user_postships_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_user_postships_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.string   "nickname"
-    t.text     "about_me"
+    t.text     "about_me",               limit: 65535
     t.string   "role"
     t.string   "fb_uid"
     t.string   "fb_token"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["fb_uid"], name: "index_users_on_fb_uid"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["fb_uid"], name: "index_users_on_fb_uid", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
